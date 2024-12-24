b8603d19a96a3e9f3f36bf760ccf4abde20ad32e
MustafaAlemdar <mustafa.alemdar200527@gmail.com>
2024-12-24 03:20:08 +0300

    Alemdar commit

---
+++ java/java.js
@@ -0,0 +1,7 @@
+// java.js
+$(document).ready(function() {
+    // Dropdown butonuna tıklama olayı
+    $('#dropdownMenuButton').on('click', function() {
+        $('.dropdown-menu').toggle();
+    });
+});
